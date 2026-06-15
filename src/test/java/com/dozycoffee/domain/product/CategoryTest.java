736f3b6e534b408f16027c99754ce2b69c12798d
yunhyeokd <yunhyeokd@gmail.com>
2026-06-15 19:13:53 +0900

    feat: Category 도메인 엔티티 및 단위 테스트 구현 (#5)

---
+++ src/test/java/com/dozycoffee/domain/product/CategoryTest.java
@@ -0,0 +1,59 @@
+package com.dozycoffee.domain.product;
+
+import com.dozycoffee.domain.common.DomainException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CategoryTest {
+
+    @Test
+    public void 카테고리를_정상_생성한다() {
+        String name = "커피";
+
+        Category category = Category.create(name);
+
+        assertThat(category.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {
+            "", " ",
+            "@커피", " 커피 ",
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    })
+    public void 카테고리_생성시_카테고리명이_유효하지_않으면_예외를_발생시킨다(String categoryName) {
+        assertThatThrownBy(() -> Category.create(categoryName))
+                .isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    public void id가_같은_카테고리는_동등하다() {
+        Category category1 = Category.of(1L, "커피1", Instant.now());
+        Category category2 = Category.of(1L, "커피2", Instant.now());
+        assertThat(category1).isEqualTo(category2);
+    }
+
+    @Test
+    public void id가_다른_카테고리는_동등하지_않다() {
+        Instant createdAt = Instant.now();
+        Category category1 = Category.of(1L, "커피1", createdAt);
+        Category category2 = Category.of(2L, "커피1", createdAt);
+        assertThat(category1).isNotEqualTo(category2);
+    }
+
+    @Test
+    public void id가_null인_카테고리는_동등하지_않다() {
+        Category category1 = Category.of(1L, "커피1", Instant.now());
+        Category category2 = Category.create("커피1");
+        assertThat(category1).isNotEqualTo(category2);
+    }
+
+}
