6155e6e50c363ad92986b3ee206b85da6c715da8
yunhyeokd <yunhyeokd@gmail.com>
2026-06-16 10:51:18 +0900

    feat: product 패키지 관계 엔티티 구현 (ProductTag, ProductOptionGroup, ProductSalesOverride) (#18)
    * feat: ProductSalesOverride 도메인 엔티티 및 단위 테스트 구현 (#17)
    
    * feat: ProductTag 도메인 엔티티 및 단위 테스트 구현 (#17)
    
    * feat: ProductOptionGroup 도메인 엔티티 및 단위 테스트 구현 (#17)
    
    * fix: 엔티티 생성 createdAt 검증 추가

---
+++ src/test/java/com/dozycoffee/domain/product/ProductOptionGroupTest.java
@@ -0,0 +1,72 @@
+package com.dozycoffee.domain.product;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductOptionGroupTest {
+
+    @Test
+    public void 상품_옵션그룹을_정상_생성한다() {
+        long productId = 1L;
+        long optionGroupId = 1L;
+        boolean isRequired = true;
+        boolean allowMultiple = false;
+
+        ProductOptionGroup productOptionGroup = ProductOptionGroup.create(productId, optionGroupId, isRequired, allowMultiple);
+
+        assertThat(productId).isEqualTo(productOptionGroup.getProductId());
+        assertThat(optionGroupId).isEqualTo(productOptionGroup.getOptionGroupId());
+        assertThat(isRequired).isEqualTo(productOptionGroup.getIsRequired());
+        assertThat(allowMultiple).isEqualTo(productOptionGroup.getAllowMultiple());
+        assertThat(productOptionGroup.getCreatedAt()).isNotNull();
+    }
+
+    @Test
+    public void id가_같은_상품_옵션그룹은_동등하다() {
+        long productId = 1L;
+        long optionGroupId = 1L;
+        boolean isRequired = true;
+        boolean allowMultiple = false;
+        Instant createdAt = Instant.now();
+
+        ProductOptionGroup productOptionGroup1 = ProductOptionGroup.of(1L, productId, optionGroupId, isRequired, allowMultiple, createdAt);
+        ProductOptionGroup productOptionGroup2 = ProductOptionGroup.of(1L, productId, optionGroupId, isRequired, allowMultiple, createdAt);
+
+        assertThat(productOptionGroup1.getId()).isEqualTo(productOptionGroup2.getId());
+        assertThat(productOptionGroup1).isEqualTo(productOptionGroup2);
+    }
+
+    @Test
+    public void id가_다른_상품_옵션그룹은_동등하지_않다() {
+        long productId = 1L;
+        long optionGroupId = 1L;
+        boolean isRequired = true;
+        boolean allowMultiple = false;
+        Instant createdAt = Instant.now();
+
+        ProductOptionGroup productOptionGroup1 = ProductOptionGroup.of(1L, productId, optionGroupId, isRequired, allowMultiple, createdAt);
+        ProductOptionGroup productOptionGroup2 = ProductOptionGroup.of(2L, productId, optionGroupId, isRequired, allowMultiple, createdAt);
+
+        assertThat(productOptionGroup1.getId()).isNotEqualTo(productOptionGroup2.getId());
+        assertThat(productOptionGroup1).isNotEqualTo(productOptionGroup2);
+    }
+
+    @Test
+    public void id가_null인_상품_옵션그룹은_동등하지_않다() {
+        long productId = 1L;
+        long optionGroupId = 1L;
+        boolean isRequired = true;
+        boolean allowMultiple = false;
+        Instant createdAt = Instant.now();
+
+        ProductOptionGroup productOptionGroup1 = ProductOptionGroup.of(1L, productId, optionGroupId, isRequired, allowMultiple, createdAt);
+        ProductOptionGroup productOptionGroup2 = ProductOptionGroup.create(productId, optionGroupId, isRequired, allowMultiple);
+
+        assertThat(productOptionGroup1.getId()).isNotEqualTo(productOptionGroup2.getId());
+        assertThat(productOptionGroup1).isNotEqualTo(productOptionGroup2);
+    }
+
+}
---
+++ src/test/java/com/dozycoffee/domain/product/ProductTagTest.java
@@ -0,0 +1,68 @@
+package com.dozycoffee.domain.product;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductTagTest {
+
+    @Test
+    public void 상품태그를_정상_생성한다() {
+        long productId = 1L;
+        long tagId = 1L;
+
+        ProductTag productTag = ProductTag.create(productId, tagId);
+
+        assertThat(productId).isEqualTo(productTag.getProductId());
+        assertThat(tagId).isEqualTo(productTag.getTagId());
+        assertThat(productTag.getCreatedAt()).isNotNull();
+    }
+
+    @Test
+    public void id가_같은_상품태그는_동등하다() {
+
+        long productId = 1L;
+        long tagId = 1L;
+        Instant createdAt = Instant.now();
+
+        ProductTag productTag1 = ProductTag.of(1L, productId, tagId, createdAt);
+        ProductTag productTag2 = ProductTag.of(1L, productId, tagId, createdAt);
+
+        assertThat(productTag1.getId()).isEqualTo(productTag2.getId());
+        assertThat(productTag1).isEqualTo(productTag2);
+    }
+
+
+    @Test
+    public void id가_다른_상품태그는_동등하지_않다() {
+
+        long productId = 1L;
+        long tagId = 1L;
+        Instant createdAt = Instant.now();
+
+        ProductTag productTag1 = ProductTag.of(1L, productId, tagId, createdAt);
+        ProductTag productTag2 = ProductTag.of(2L, productId, tagId, createdAt);
+
+        assertThat(productTag1.getId()).isNotEqualTo(productTag2.getId());
+        assertThat(productTag1).isNotEqualTo(productTag2);
+    }
+
+    @Test
+    public void id가_null인_상품태그는_동등하지_않다() {
+
+        long productId = 1L;
+        long tagId = 1L;
+        Instant createdAt = Instant.now();
+
+        ProductTag productTag1 = ProductTag.of(1L, productId, tagId, createdAt);
+        ProductTag productTag2 = ProductTag.create(productId, tagId);
+
+        assertThat(productTag1.getId()).isNotEqualTo(productTag2.getId());
+        assertThat(productTag1).isNotEqualTo(productTag2);
+    }
+
+
+
+}
