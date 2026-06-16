cd566f661c6dba738219dd564629a9ead3ca24dd
yunhyeokd <yunhyeokd@gmail.com>
2026-06-16 15:26:52 +0900

    feat: Repository 전용 예외 작성 및 Service 예외코드 DomainCode 기반으로 변경

---
+++ src/main/java/com/dozycoffee/application/common/RepositoryException.java
@@ -0,0 +1,24 @@
+package com.dozycoffee.application.common;
+
+import com.dozycoffee.domain.common.DomainCode;
+
+public class RepositoryException extends RuntimeException {
+    public RepositoryException() {
+    }
+
+    public RepositoryException(String message) {
+        super(message);
+    }
+
+    public RepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RepositoryException(Throwable cause) {
+        super(cause);
+    }
+
+    public RepositoryException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
