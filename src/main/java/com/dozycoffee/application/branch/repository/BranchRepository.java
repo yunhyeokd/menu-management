54571b243fe52568472f8b998904de7e7e67149b
yunhyeokd <yunhyeokd@gmail.com>
2026-06-19 12:00:40 +0900

    fix: BranchRepository 제거 및 BranchService 스켈레톤 정리 (Branch 도메인 분리 후 컴파일 오류 수정)

--- src/main/java/com/dozycoffee/application/branch/repository/BranchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dozycoffee.application.branch.repository;
-
-import com.dozycoffee.domain.branch.Branch;
-
-public interface BranchRepository {
-    Branch findByName(String branchName);
-
-    Branch save(Branch branch);
-}
--- src/main/java/com/dozycoffee/application/branch/service/BranchService.java
+++ src/main/java/com/dozycoffee/application/branch/service/BranchService.java
@@ -1,43 +1,4 @@
 package com.dozycoffee.application.branch.service;
 
-import com.dozycoffee.application.branch.dto.BranchProfileUpdateDto;
-import com.dozycoffee.application.branch.repository.BranchProfileRepository;
-import com.dozycoffee.application.common.RepositoryException;
-import com.dozycoffee.application.product.repository.ProductRepository;
-import com.dozycoffee.domain.branch.BranchException;
-import com.dozycoffee.domain.branch.BranchProfile;
-
 public class BranchService {
-
-    private final BranchProfileRepository branchProfileRepository;
-    private final ProductRepository productRepository;
-
-    public BranchService(
-            BranchProfileRepository branchProfileRepository,
-            ProductRepository productRepository
-    ) {
-        this.branchProfileRepository = branchProfileRepository;
-        this.productRepository = productRepository;
-    }
-
-    public void updateProfile(long branchId, BranchProfileUpdateDto updateDto) {
-
-        try {
-            BranchProfile branchProfile = branchProfileRepository.findById(branchId);
-            if (branchProfile == null) {
-                throw BranchBusinessException.with(BranchErrors.NOT_FOUND_ERROR);
-            }
-            try {
-                branchProfile.changeName(updateDto.name());
-                branchProfile.changeAddress(updateDto.address());
-                branchProfileRepository.save(branchProfile);
-            } catch (BranchException e) {
-                throw BranchBusinessException.with(BranchErrors.INVALID_BRANCH_ERROR);
-            }
-        } catch (RepositoryException e) {
-            throw BranchBusinessException.with(BranchErrors.UNKNOWN_ERROR);
-        }
-
-    }
-
 }
