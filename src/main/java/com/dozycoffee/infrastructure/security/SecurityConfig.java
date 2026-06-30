4444959e3a0890b52b1603b436b4b95f7c0446e0
yunhyeokd <yunhyeokd@gmail.com>
2026-06-30 17:50:59 +0900

    refactor: SecurityConfig에서 Argon2PasswordEncoder 익명객체로 직접 주입

--- src/main/java/com/dozycoffee/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/dozycoffee/infrastructure/security/SecurityConfig.java
@@ -3,13 +3,26 @@ package com.dozycoffee.infrastructure.security;
 import com.dozycoffee.auth.application.PasswordHasher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public PasswordHasher passwordHasher() {
-        return new Argon2PasswordHasher();
+        return new PasswordHasher() {
+            final Argon2PasswordEncoder passwordEncoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+
+            @Override
+            public String hash(String raw) {
+                return passwordEncoder.encode(raw);
+            }
+
+            @Override
+            public boolean matches(String raw, String hash) {
+                return passwordEncoder.matches(raw, hash);
+            }
+        };
     }
 
 
